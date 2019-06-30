4641a052fd56c88add1e2dfc831169bddd1af37c
mukund109 <mukund.chaudhry@gmail.com>
2019-06-30 17:56:42 +0530

    updated solution to 20

--- 20. Maximal Square.cpp
+++ 20. Maximal Square.cpp
@@ -1,74 +1,46 @@
 // https://leetcode.com/problems/maximal-square/
 
-
+// O(n) space, O(mn) time
 class Solution {
-private:
-	
-	int rec_maximal(vector<vector<char>> matrix, int depth){
-		int height = matrix.size();
-		int width = matrix[0].size();
-		
-		if(!non_zero(matrix))
-			return depth-1;
-			
-		if(height == 2){
-			for(int i =0; i<width-1; i++){
-				if(is_square(matrix, i, 0))
-					return depth+1;
-			}
-			if (non_zero(matrix))
-				return depth;
-			return depth-1;
-		}
-		else if(width==2){
-			for(int i=0; i<height-1; i++){
-				if(is_square(matrix, 0, i))
-					return depth+1;
-			}
-			if (non_zero(matrix))
-				return depth;
-			return depth-1;
-		}
-		vector<vector<char>> next_mat(height-1, vector<char>(width-1, '1'));
-		
-		for(int y = 0; y<height-1; y++){
-			for(int x = 0; x<width-1; x++){
-				if(!is_square(matrix, x, y))
-					next_mat[y][x] = '0';
-			}
-		}
-		return rec_maximal(next_mat, depth+1);
-	}
 public:
+    	int maximalSquare(vector<vector<char>>& matrix){
+            int M = matrix.size();
+            if(M==0)
+                return 0;
 
-	bool non_zero(vector<vector<char>>& mat){
-		for (auto& e: mat){
-			for(auto& g: e)
-				if(g=='1')
-					return true;
+            int N = matrix[0].size();
+            vector<int> bottom(N+1, 0);
+            vector<int> current_row(N+1, 0);
+
+            int right;
+            int max_side = 0;
+            for(int y = M-1; y>=0; y--){
+                right = 0;
+                for(int x = N-1; x>=0; x--){
+                    if(matrix[y][x]=='1'){
+                        current_row[x] = min(min(bottom[x], bottom[x+1]), right) + 1;
+                        max_side = (current_row[x]>max_side) ? current_row[x]: max_side;
+                        right++;
+                    }
+                    else{
+                        current_row[x] = 0;
+                        right = 0;
+                    }
+                }
+                bottom.swap(current_row);
 		}
-		return false;
+		return max_side*max_side;
 	}
-	
-	bool is_square(vector<vector<char>>& mat, int x, int y){
-		bool rv = mat[y][x] == '1' and mat[y][x+1] == '1';
-		rv = rv and mat[y+1][x]=='1' and mat[y+1][x+1]=='1';
-		return rv;
-	}
-	
-    int maximalSquare(vector<vector<char>>& matrix) {
-		if(matrix.size()==0)
-			return 0;
-		bool nonzero = non_zero(matrix);
-		if (!nonzero)
-			return 0;
-		if (matrix.size()==1 || matrix[0].size()==1){
-			if(nonzero)
-				return 1;
-			return 0;
-		}
-			
-		int side = rec_maximal(matrix, 1);
-		return side*side;
-    }
 };
+
+int main(){
+	vector<vector<char>> b({{'0','0'},{'1','1'}, {'1','1'}});
+	vector<vector<char>> c({{'0'},{'0'}});
+	vector<vector<char>> d({{'0', '1'}});
+	vector<vector<char>> e({{'1','1','1','1','1'},{'1','1','1','1','1'},{'1','1','1','1','1'},{'1','1','1','1','1'}});
+	vector<vector<char>> f({{'0','0','0','0','0'},{'1','0','0','0','0'},{'0','0','0','0','0'},{'0','0','0','0','0'}});
+	vector<vector<char>> g;
+	cout << Solution().maximalSquare(b) << endl;
+}
+
+
