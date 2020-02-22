f5f5eba538b53abc6310b481b395bcb160084710
mukund109 <mukund.chaudhry@gmail.com>
2020-02-22 21:39:44 +0530

    cleaner, optimized soln to 51

--- 51.LongestPalindromicSubstring(M).py
+++ 51.LongestPalindromicSubstring(M).py
@@ -1,50 +1,45 @@
 class Solution:
 
     def longestPalindrome(self, s: str) -> str:
-        self.string = s
+
+        def isPalindrome(s, m, n):
+            # returns largest palindrome in s with m,n as its center
+
+            while m >= 0 and n < len(s):
+                if s[m] != s[n]:
+                    break
+                m -= 1
+                n += 1
+            return s[m+1:n]
+        
+        cl, cr = len(s)//2 - 1 , len(s)//2
         rv = ''
-        for i in range(len(self.string)):
-            newPalindrome = self.isPalindrome(i)
-            if len(newPalindrome) > len(rv):
-                rv = newPalindrome
+        if len(s)%2==1:
+            rv = isPalindrome(s, cl, cl)
+            cl += 1
+
+        for r,l in zip(range(cr,len(s)), range(cl,-1,-1)):
+
+            if len(rv) > 2*l+1:
+                return rv
+
+            rv = max(rv,
+                    isPalindrome(s, l, l),
+                    isPalindrome(s, l, l+1),
+                    isPalindrome(s, r, r),
+                    isPalindrome(s, r, r-1),
+                    key = len)
+            
         return rv
 
-    def isPalindrome(self, center):
-        
-        # returns largest palindrome in self.string with given center
-
-        if (center < 0 or center >= len(self.string)):
-            return ''
-
-        l = 0
-        while center-l-1 >=0 and center+l+1 < len(self.string):
-            if self.string[center-l-1] == self.string[center+l+1]:
-                l += 1
-            else:
-                break
-
-        # even case 
-        k = 0
-        while (center-k >= 0) and (center+k+1 < len(self.string)):
-            if self.string[center-k] == self.string[center+k+1]:
-                k += 1
-            else:
-                break
-        # print( "l:",l," k:",k)
-        if l>=k:
-            return self.string[center-l: center+l+1]
-        else:
-            return self.string[center+1-k: center+k+1]
 
 if __name__=="__main__":
     solution = Solution()
-    solution.string = "abcdcba"
-    print(solution.isPalindrome(0), "zeroth")
-    print(solution.isPalindrome(1))
-    print(solution.isPalindrome(2))
-    print(solution.isPalindrome(3))
-    print(solution.isPalindrome(4))
-    print(solution.isPalindrome(5))
-    print(solution.isPalindrome(6))
-    print(solution.isPalindrome(7))
     print(solution.longestPalindrome("abadaba"))
+    print(solution.longestPalindrome("calskdfn"))
+    print(solution.longestPalindrome("bb"))
+    print(solution.longestPalindrome("a"))
+    print(solution.longestPalindrome("ccc"))
+    print(solution.longestPalindrome("dddd"))
+    print(solution.longestPalindrome("aacdefcaa"))
+    print(solution.longestPalindrome("cbbd"))
